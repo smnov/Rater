f5386febbfa9e1235749d82a1530cb609e41bae8
aleksandr semenov <aleksandr.semenovsky@gmail.com>
2023-11-14 18:59:09 +0800

    Initial Commit

---
+++ Rater/RaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaterApp.swift
+//  Rater
+//
+//  Created by Александр Семенов on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
